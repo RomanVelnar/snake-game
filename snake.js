3fe7ba90451217ddab14a5e8dfb8fe5a3c097783
RomanVelnar <roman.velnar@gmail.com>
2022-03-04 17:14:33 +0000

    finished with the snake movement and food. Added random food spawns, missing on how to end the game. Further need to expand on levels

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 import { getInputDirection } from "./input.js"
 
 export const SNAKE_SPEED = 5
-const snakeBody = [{x: 10, y: 11}]
+const snakeBody = [{x: 11, y: 11}]
 let newSegments = 0
 
 export function update() {
