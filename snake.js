378548d761d411968a9025864ab61837a113a9f4
RomanVelnar <roman.velnar@gmail.com>
2022-03-04 15:06:29 +0000

    created snake keystroke movement

--- snake.js
+++ snake.js
@@ -1,11 +1,7 @@
 import { getInputDirection } from "./input.js"
 
 export const SNAKE_SPEED = 1
-const snakeBody = [
-    {x: 10, y: 11},
-    {x: 11, y: 11},
-    {x: 12, y: 11}
-]
+const snakeBody = [{x: 10, y: 11}]
 
 export function update() {
     const inputDirection = getInputDirection()
